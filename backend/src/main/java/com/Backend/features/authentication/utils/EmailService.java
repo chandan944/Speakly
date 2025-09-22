49cb3977f1fde728c611039ad11b1665725fa08e
chandan prajapati <chandanprajapati6307@>
2025-09-22 20:50:05 +0530

    Updated email service and properties + added image

--- backend/src/main/java/com/Backend/features/authentication/utils/EmailService.java
+++ backend/src/main/java/com/Backend/features/authentication/utils/EmailService.java
@@ -1,75 +1,32 @@
 package com.Backend.features.authentication.utils;
-//
-//import jakarta.mail.MessagingException;
-//import jakarta.mail.internet.MimeMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.UnsupportedEncodingException;
-//
-//@Service
-//public class EmailService {
-//    private final JavaMailSender mailSender;
-//
-//    public EmailService(JavaMailSender mailSender) {
-//        this.mailSender = mailSender;
-//    }
-//
-//
-//    public void sendEmail(String email , String subject ,String content ) throws MessagingException , UnsupportedEncodingException {
-//        MimeMessage message = mailSender.createMimeMessage();
-//
-//        MimeMessageHelper helper = new MimeMessageHelper(message);
-//        helper.setFrom("no-reply@linkdin.com" , "Speakly");
-//        helper.setTo(email);
-//        helper.setSubject(subject);
-//        helper.setText(content ,true);
-//
-//        mailSender.send(message);
-//    }
-//}
 
-
-import com.sendgrid.*;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
 
+    public void sendEmail(String email , String subject ,String content ) throws MessagingException , UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
 
-    public void sendEmail(String toEmail, String subject, String htmlContent) throws IOException {
-        Email from = new Email();  // sender
-        Email to = new Email(toEmail);                // recipient
-        Content content = new Content("text/html", htmlContent);
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom("no-reply@linkdin.com" , "Speakly");
+        helper.setTo(email);
+        helper.setSubject(subject);
+        helper.setText(content ,true);
 
-        Mail mail = new Mail(from, subject, to, content);
-
-        // Store API key in application.properties
-        String sendGridApiKey = "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";
-        SendGrid sg = new SendGrid(sendGridApiKey);
-        Request request = new Request();
-
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sg.api(request);
-
-            System.out.println("Status Code: " + response.getStatusCode());
-            System.out.println("Response Body: " + response.getBody());
-            System.out.println("Response Headers: " + response.getHeaders());
-
-        } catch (IOException ex) {
-            throw ex;
-        }
+        mailSender.send(message);
     }
 }
 
